f52c96066fd5a5df84f80c2ba863550c2cacb8da
wuzhiqun <wuzhiqun@ibona.cn>
2018-12-01 20:57:43 +0800

    初始化

---
+++ less1/src/main/java/com/example/less1/Less1Application.java
@@ -0,0 +1,12 @@
+package com.example.less1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Less1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Less1Application.class, args);
+	}
+}
---
+++ less1/src/test/java/com/example/less1/Less1ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.less1;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Less1ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
